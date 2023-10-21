9db3199954dfe13b22994b8f8911e8fafb4c2c9a
Vallencius <vallencius.siswanto@student.umn.ac.id>
2023-10-21 18:10:43 +0700

    Add back button

--- static/color.js
+++ static/color.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     function searchWord() {
-        console.log("ASD")
         var result = document.getElementById('result').innerText;
         var True = true;
         var False = false;
-        var None = null;
+        var None = null
         console.log(result)
         var resultValue = eval('(' + result + ')');
         
@@ -13,13 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
         var newParagraph = document.getElementById('newParagraph');
         var fixContent = newParagraph.innerHTML;
 
-        console.log(resultValue)
-
         for (var key in resultValue) {
             var suggestion = resultValue[key].suggestion;
             var is_correct = resultValue[key].is_correct;
             if (is_correct) {
                 content = content.replace(new RegExp(key, 'g'), '<span class="font-bold text-green-500">$&</span>');
+                fixContent = fixContent.replace(new RegExp(key, 'g'), '<span class="font-bold text-green-500">$&</span>');
             } else {
                 content = content.replace(new RegExp(key, 'g'), '<span class="font-bold text-red-500">$&</span>');
                 fixContent = fixContent.replace(new RegExp(key, 'g'), '<span class="font-bold text-blue-700">' + suggestion + '</span>');
